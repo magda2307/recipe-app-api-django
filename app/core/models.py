1a67ed76e4ca99ff6f333c95bee31af898f60cc2
Magda Sokolowska <magdalenasokolowska2307@gmail.com>
2023-05-29 21:55:43 +0200

    create user model.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+"""
+Database models for project.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
