ff78e6bf0b46cbc017a00f177f239d7ae27ecbde
Magda Sokolowska <magdalenasokolowska2307@gmail.com>
2023-05-30 15:19:19 +0200

    add custom display of admin page for users.

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,13 +5,27 @@ Django admin customization.
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.translation import gettext_lazy as translate
 from core import models
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
-    
+    fieldsets = (
+        (translate('General data'), {'fields':('email','password')}),
+        (
+            translate('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (translate('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
     
 admin.site.register(models.User, UserAdmin)
