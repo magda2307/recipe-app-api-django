168c8427cade153e1f3e6f8a38c2bea99222c8ed
Magda Sokolowska <magdalenasokolowska2307@gmail.com>
2023-05-30 15:37:25 +0200

    add adding user by admin interface and tests.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -29,14 +29,21 @@ class AdminSiteTests(TestCase):
     def test_users_list(self):
         """Test whether the users are listed on page."""
         url = reverse('admin:core_user_changelist')
-        response = self.client.get(url)
+        res = self.client.get(url)
         
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.user.name)
-        self.assertContains(response, self.user.email)
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
         
     def test_edit_user_page(self):
         """Test if the edit user page works."""
         url = reverse("admin:core_user_change", args=[self.user.id])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+        
+    def test_create_user_page(self):
+        """Test if the create user page works."""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+        
+        self.assertEqual(res.status_code, 200)
